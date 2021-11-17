f70407bef4f1d83dfa9f41f5cc7b85837588a068
attachemd <mattache.vivace@gmail.com>
2021-11-17 12:07:36 +0100

    Creating JSON Response - All Elements.

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from watchlist_app.models import Movie
+
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+    }
+    return JsonResponse(data)
