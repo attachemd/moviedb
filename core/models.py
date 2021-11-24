ea14562037b7d86f03717f65cdb33249f022b4f7
attachemd <mattache.vivace@gmail.com>
2021-11-24 17:16:29 +0100

    Expand models.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -24,3 +25,19 @@ class WatchListModel(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    watchlist = models.ForeignKey(
+        WatchListModel,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.rating
